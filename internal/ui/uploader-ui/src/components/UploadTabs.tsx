1240fb9e61dc1a3e2cc06dcfe4b3a2f29114f933
Mike Wyatt <wyatt.mike@gmail.com>
2025-12-08 19:37:33 -0400

    I'm calling this the uploader rework

--- internal/ui/uploader-ui/src/components/UploadTabs.tsx
+++ internal/ui/uploader-ui/src/components/UploadTabs.tsx
@@ -1,29 +1,34 @@
 import Box from 'shared/Box.tsx'
 import Button from 'shared/Button.tsx'
+import type { Component } from 'solid-js'
 
 interface UploadTabsProps {
   activeTab: 'file' | 'text'
   onTabChange: (tab: 'file' | 'text') => void
 }
 
-export default function UploadTabs(props: UploadTabsProps) {
+const UploadTabs: Component<UploadTabsProps> = (props) => {
   return (
-    <Box display="flexRow" gap="x1" marginBottom="x2">
+    <Box display="flexRow" flexAlign="centerAll" gap="x1" marginBottom="x2">
       <Button
         size="medium"
         color={props.activeTab === 'file' ? 'primary' : 'secondary'}
         onClick={() => props.onTabChange('file')}
+        style={{ 'flex-basis': '50%' }}
       >
-        Files
+        Pictures ...
       </Button>
 
       <Button
         size="medium"
         color={props.activeTab === 'text' ? 'primary' : 'secondary'}
         onClick={() => props.onTabChange('text')}
+        style={{ 'flex-basis': '50%' }}
       >
-        Text
+        Messages ...
       </Button>
     </Box>
   )
 }
+
+export default UploadTabs
